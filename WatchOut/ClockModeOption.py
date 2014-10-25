3cba73ec90e06bcbb22e667a230ae1136bd36918
christianhill <christian.hill@esendex.com>
2014-10-25 19:17:35 +0100

    This branch can now display the time!
    Added basic time-printing logic to ClockModeOption. Does not automatically update. Yet.

--- WatchOut/ClockModeOption.py
+++ WatchOut/ClockModeOption.py
@@ -1,11 +1,13 @@
 __author__ = 'Jonathan'
 from WatchOut.ModeOption import ModeOption
+import time
 
 class ClockModeOption(ModeOption):
     def __init__(self,*args):
         super().__init__(*args)
 
     def enter(self):
-        self.update_display("hello world")
-        pass
+        timestr = time.strftime("%H:%M:%S")
+        datestr = time.strftime("%a %d-%m-%y")
+        self.update_display(timestr+"\n"+datestr)
     
